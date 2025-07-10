10eefbb751aa28e5344b712ab2f4b7afda29df44
Izaan Mohamed <izaan.mohamed@gmail.com>
2025-07-10 09:50:18 -0500

    deleted some code

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-// Block represents each 'item' in the blockchain
+
 type Block struct {
 	Index    int
 	Data     string
@@ -14,16 +14,16 @@ type Block struct {
 	Hash     string
 }
 
-// calculateHash creates a SHA-256 hash of a block
+
 func calculateHash(block Block) string {
-	// Correct way to format all parts as a string
+	
 	record := fmt.Sprintf("%d%s%s", block.Index, block.Data, block.PrevHash)
 	hash := sha256.New()
 	hash.Write([]byte(record))
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
-// createBlock creates a new block using the previous block’s hash
+
 func createBlock(prevBlock Block, data string) Block {
 	newBlock := Block{
 		Index:    prevBlock.Index + 1,
@@ -34,7 +34,7 @@ func createBlock(prevBlock Block, data string) Block {
 	return newBlock
 }
 
-// printBlock prints the details of a block
+
 func printBlock(block Block) {
 	fmt.Printf("Block #%d\n", block.Index)
 	fmt.Printf("Data: %s\n", block.Data)
@@ -43,15 +43,15 @@ func printBlock(block Block) {
 }
 
 func main() {
-	// Create genesis block
+
 	genesisBlock := Block{Index: 0, Data: "Genesis Block", PrevHash: ""}
 	genesisBlock.Hash = calculateHash(genesisBlock)
 
-	// Create additional blocks
+
 	secondBlock := createBlock(genesisBlock, "Second Block")
 	thirdBlock := createBlock(secondBlock, "Third Block")
 
-	// Print the blockchain
+	
 	printBlock(genesisBlock)
 	printBlock(secondBlock)
 	printBlock(thirdBlock)
